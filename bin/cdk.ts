8b55008ed1bc7ca327a1e07d154ec6f32aeb6911
Ruben Fonseca <fonseka@gmail.com>
2024-02-01 14:24:30 +0100

    update placeholders

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-import * as cdk from 'aws-cdk-lib';
-import { AppStage } from '../lib/app-stage';
-import { PipelineStack } from '../lib/pipeline-stack';
+import * as cdk from "aws-cdk-lib";
+import { AppStage } from "../lib/app-stage";
+import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
 
-const delivery = new PipelineStack(app, 'Api-DeliveryPipeline', {
-  name: 'Api',
+const delivery = new PipelineStack(app, "Api-DeliveryPipeline", {
+  name: "Api",
   env: {
-    account: '12345678910',
-    region: 'us-east-1'
-  }
+    account: "715056256865",
+    region: "eu-north-1",
+  },
 });
 
 delivery.pipeline.addStage(
-  new AppStage(app, 'App', {
+  new AppStage(app, "App", {
     env: {
-      account: '01987654321',
-      region: 'us-east-1',
+      account: "715056256865",
+      region: "eu-north-1",
     },
-  })
+  }),
 );
--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,8 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-import * as cdk from 'aws-cdk-lib';
-import * as pipelines from 'aws-cdk-lib/pipelines'
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as pipelines from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 
 export interface PipelineStackProps extends cdk.StackProps {
   name: string;
@@ -14,15 +14,16 @@ export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
 
-    this.pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      synth: new pipelines.ShellStep('synth', {
-        input: pipelines.CodePipelineSource.connection('REPLACE_WITH_OWNER/cdk-bundle-go-lambda-example', 'main', {
-          connectionArn: 'REPLACE_WITH_CONNECTION_ARN',
-        }),
-        commands: [
-          'npm ci',
-          'npx cdk synth',
-        ],
+    this.pipeline = new pipelines.CodePipeline(this, "Pipeline", {
+      synth: new pipelines.ShellStep("synth", {
+        input: pipelines.CodePipelineSource.connection(
+          "rubenfonseca/cdk-bundle-go-lambda-example",
+          "main",
+          {
+            connectionArn: "REPLACE_WITH_CONNECTION_ARN",
+          },
+        ),
+        commands: ["npm ci", "npx cdk synth"],
       }),
       crossAccountKeys: true,
       dockerEnabledForSynth: true,
